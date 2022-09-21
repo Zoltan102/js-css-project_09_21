27726c3d0626b4480b07760836d6114d488a905b
Zoltan102 <suveges.zoltan@petrik.hu>
2022-09-21 12:52:53 +0200

    button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,23 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
 
+function init(){
+    document.getElementById('colourChanger').addEventListener('click', colourChange);
+}
+
+let colours = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'cyan',
+    'purple',
+];
+
+function colourChange(){
+    console.log('katt');
+}
+document.addEventListener('DOMContentLoaded', init);
+
 console.log('Loaded');
